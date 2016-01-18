4b1f2f993caa20e9fc4a517feb270695e0606e56
Vivek Mangla <vivekFuneesh@users.noreply.github.com>
2016-01-18 15:42:23 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,19 +21,8 @@ public class avlWithRoot {
 /*******************************************************************
 
     Main.java is part of AVL_TREE.
-
-     AVL_TREE is free code package: you can redistribute it    and/or modify
-    it under the terms of the GNU General Public License as published     by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-   AVL_TREE is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with AVL_TREE.  If not, see <http://www.gnu.org/licenses/>.
+    This is distributed under MIT Licence.
+    It was previously distributed under GPLv3 .
 
 *******************************************************************
 */
